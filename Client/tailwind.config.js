224eb06a9b83622963890f48d0a706370dc5cfd2
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2023-07-28 23:08:59 +0530

    update

---
+++ Client/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    theme: {
+      // Your theme configuration here
+    },
+    variants: {},
+    plugins: [],
+  };
+  
