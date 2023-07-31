96a41a1aaff939c7d68ffceeed0ed0fff40fdb29
Diptanshu bhawsar <Diptanshubhawsar50@gmail.com>
2023-07-31 20:48:10 +0530

    remove unwanted filed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="text-align-left">
+      <div className="text-align-left font-bold m-5">
         diptanshu bhawsar 
       </div>
     </div>
